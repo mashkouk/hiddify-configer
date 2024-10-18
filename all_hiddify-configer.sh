da910b27320663d83c0ebe78bcc1e830cccf6ae5
mashkoukras ( reza as ) <55289441+mashkouk@users.noreply.github.com>
2024-10-18 14:12:40 +0330

    Add files via upload

---
+++ all_hiddify-configer.sh
@@ -0,0 +1,3 @@
+bash <(curl -Ls https://raw.githubusercontent.com/mashkouk/hiddify-configer/refs/heads/main/fragment_tiktok_user_fixer.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/mashkouk/hiddify-configer/refs/heads/main/xray_core_changer.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/mashkouk/hiddify-configer/refs/heads/main/dns_changer.sh)
