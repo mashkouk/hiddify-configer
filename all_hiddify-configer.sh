87c60dc9c90498cdb87c6141e76e6f90be830fb3
mashkouk <55289441+mashkouk@users.noreply.github.com>
2024-11-25 18:12:37 +0330

    Update all_hiddify-configer.sh

--- all_hiddify-configer.sh
+++ all_hiddify-configer.sh
@@ -1,3 +1,5 @@
 bash <(curl -Ls https://raw.githubusercontent.com/mashkouk/hiddify-configer/refs/heads/main/fragment_tiktok_user_fixer.sh)
+clear
 bash <(curl -Ls https://raw.githubusercontent.com/mashkouk/hiddify-configer/refs/heads/main/xray_core_changer.sh)
+clear
 bash <(curl -Ls https://raw.githubusercontent.com/mashkouk/hiddify-configer/refs/heads/main/dns_changer.sh)
