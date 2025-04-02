83d02005a0d783609c6e3bf54a10aa12854ac73a
mashkouk <55289441+mashkouk@users.noreply.github.com>
2025-04-03 01:13:08 +0330

    Update fragment_tiktok_user_fixer.sh

--- fragment_tiktok_user_fixer.sh
+++ fragment_tiktok_user_fixer.sh
@@ -6,7 +6,6 @@ clear
 # تغییر مقدار خط 15-34-6 از true به false
 sed -i '6s/warning/none/' base_xray_config.json.j2
 sed -i '15s/true/false/' base_xray_config.json.j2
-sed -i '34s/true/false/' base_xray_config.json.j2
 clear
 # پیشنهادات برای جایگزینی عبارت tlshello در خط 56
 echo "mizan packets fragment ra vared va ya az zir entekhab konid:"
