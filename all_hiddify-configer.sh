27573809600469270333aed437a25d9caa6d474e
mashkouk <55289441+mashkouk@users.noreply.github.com>
2025-04-24 20:09:42 +0330

    Update all_hiddify-configer.sh

--- all_hiddify-configer.sh
+++ all_hiddify-configer.sh
@@ -2,4 +2,7 @@ bash <(curl -Ls https://raw.githubusercontent.com/mashkouk/hiddify-configer/refs
 clear
 bash <(curl -Ls https://raw.githubusercontent.com/mashkouk/hiddify-configer/refs/heads/main/xray_core_changer.sh)
 clear
+bash <(curl -Ls https://raw.githubusercontent.com/mashkouk/hiddify-configer/refs/heads/main/cronjob_apply_config.sh)
+clear
 bash <(curl -Ls https://raw.githubusercontent.com/mashkouk/hiddify-configer/refs/heads/main/dns_changer.sh)
+
